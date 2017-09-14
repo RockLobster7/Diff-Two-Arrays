2b07dd7788a41d61d1118408fe455fe432bd2e44
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-14 21:25:35 +1000

    commented inn an alternate solution

--- Diff-Two-Arrays.js
+++ Diff-Two-Arrays.js
@@ -21,6 +21,14 @@ function diffArray(arr1, arr2) {
 }
 
 
+//alternative solution i didnt write
+// function diffArray(arr1, arr2) {
+//   return arr1.concat(arr2).filter(
+//    	item => !arr1.includes(item) || !arr2.includes(item)
+//    );
+// }
+
+
 console.log(diffArray ([1, 2, 3, 5], [1, 2, 3, 4, 5])); //should return an array.
 console.log(diffArray (["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])); // should return ["pink wool"].
 console.log(diffArray (["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])); //should return ["diorite", "pink wool"].
